2e41d781d34d47bcf17572b0e3e56ae8b91e3099
Georgio Mazraani <mazraanigeorgio@gmail.com>
2024-07-31 19:31:38 +0300

    Adding CORS and updating the excel data file

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const xlsx = require('xlsx');
+var cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors())
 
-
-let wb = xlsx.readFile('students.xlsx');
+let wb = xlsx.readFile('employees.xlsx');
 let ws = wb.Sheets['Sheet1'];
 
+let data = xlsx.utils.sheet_to_json(ws);
+
 app.get('/', (req, res) => {
-    res.send("Hey")
+    res.send("Hello World")
 })
 
 app.get('/students', (req, res) => {
-    let data = xlsx.utils.sheet_to_json(ws);
     res.send(data)
 })
 
