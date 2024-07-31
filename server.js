5f4ad96b5603453e68f9352dda9a864ad518f7fe
Georgio Mazraani <mazraanigeorgio@gmail.com>
2024-07-31 18:57:54 +0300

    Adding new Endpoint

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ app.get('/', (req, res) => {
     res.send("Hey")
 })
 
+app.get('/students', (req, res) => {
+    var data = [
+        { id: 1, name: 'Georgio Mazraani' },
+        { id: 2, name: 'Sarah Al Najjar' },
+        { id: 3, name: 'Marina Mazraani' },
+        { id: 4, name: 'David Mazraani' }
+    ];
+    res.send(data)
+})
+
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`);
 });
